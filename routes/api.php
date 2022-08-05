d6161f38e02321755f65e252d424a2d5d3534bd0
luka <lukakurdadze2@gmail.com>
2022-08-05 17:23:34 +0400

    refactor: use named routes

--- routes/api.php
+++ routes/api.php
@@ -26,53 +26,53 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware(['guest'])->group(function () {
-	Route::post('/register/create', [AuthController::class, 'register']);
-	Route::post('/login', [AuthController::class, 'login']);
+	Route::post('/register/create', [AuthController::class, 'register'])->name('register');
+	Route::post('/login', [AuthController::class, 'login'])->name('login');
 
-	Route::post('/forget-password', [PasswordResetController::class, 'submitForgetPasswordForm']);
-	Route::post('/reset-password', [PasswordResetController::class, 'submitResetPasswordForm']);
+	Route::post('/forget-password', [PasswordResetController::class, 'submitForgetPasswordForm'])->name('password.forget');
+	Route::post('/reset-password', [PasswordResetController::class, 'submitResetPasswordForm'])->name('password.reset');
 
 	Route::group(['middleware' => ['web']], function () {
-		Route::get('/auth/redirect', [OAuthController::class, 'redirect']);
-		Route::get('/google-callback', [OAuthController::class, 'callback']);
+		Route::get('/auth/redirect', [OAuthController::class, 'redirect'])->name('google.auth-redirect');
+		Route::get('/google-callback', [OAuthController::class, 'callback'])->name('google.callback');
 	});
 });
 	Route::get('email/verify/{id}', [EmailVerificationController::class, 'verify'])->name('verification.verify');
 
 Route::middleware(['auth:api'])->group(function () {
-	Route::post('/logout', [AuthController::class, 'logout']);
+	Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
-	Route::get('/user', [UserController::class, 'index']);
-	Route::post('/user', [UserController::class, 'update']);
+	Route::get('/user', [UserController::class, 'index'])->name('user.get');
+	Route::post('/user', [UserController::class, 'update'])->name('user.update');
 
 	Route::controller(MovieController::class)->group(function () {
-		Route::get('/movies', 'index');
-		Route::get('/movies/{movie:slug}', 'show');
-		Route::get('/movies/{movie:slug}/edit', 'edit');
-		Route::post('/movies/{movie:slug}/edit', 'update');
-		Route::post('/movies/{movie:slug}/remove', 'destroy');
-		Route::post('/movies/add', 'store');
+		Route::get('/movies', 'index')->name('movies.get');
+		Route::get('/movies/{movie:slug}', 'show')->name('movies.show');
+		Route::get('/movies/{movie:slug}/edit', 'edit')->name('movies.edit');
+		Route::post('/movies/{movie:slug}/edit', 'update')->name('movies.update');
+		Route::post('/movies/{movie:slug}/remove', 'destroy')->name('movies.destroy');
+		Route::post('/movies/add', 'store')->name('movies.store');
 	});
 
 	Route::controller(QuoteController::class)->group(function () {
-		Route::get('/quotes', 'index');
-		Route::get('/movies/{movie:slug}/quote/{quote:id}', 'show');
-		Route::post('/movies/{movie:slug}/quote/{quote:id}', 'update');
-		Route::post('/quotes/create', 'store');
-		Route::post('/movies/{movie:slug}/quote/{quote:id}/remove', 'destroy');
+		Route::get('/quotes', 'index')->name('quotes.get');
+		Route::get('/movies/{movie:slug}/quote/{quote:id}', 'show')->name('quotes.show');
+		Route::post('/movies/{movie:slug}/quote/{quote:id}', 'update')->name('quotes.update');
+		Route::post('/quotes/create', 'store')->name('quotes.store');
+		Route::post('/movies/{movie:slug}/quote/{quote:id}/remove', 'destroy')->name('quotes.destroy');
 	});
 
 	Route::controller(NotificationController::class)->group(function () {
-		Route::post('/notify-user', 'index');
-		Route::get('/notifications', 'getUserNotifications');
-		Route::post('/notifications/read-all', 'updateNotifications');
+		Route::post('/notify-user', 'index')->name('notifications.get');
+		Route::get('/notifications', 'getUserNotifications')->name('notifications.get-user-notifications');
+		Route::post('/notifications/read-all', 'updateNotifications')->name('notifications.update-notifications');
 	});
 
-	Route::get('/genres', [GenreController::class, 'index']);
+	Route::get('/genres', [GenreController::class, 'index'])->name('genres.get');
 
-	Route::post('/search', [SearchController::class, 'search']);
+	Route::post('/search', [SearchController::class, 'search'])->name('search');
 
-	Route::post('/comment/add', [CommentController::class, 'index']);
-	Route::post('/like/add', [LikeController::class, 'index']);
-	Route::post('/like/remove', [LikeController::class, 'destroy']);
+	Route::post('/comment/add', [CommentController::class, 'index'])->name('comment.store');
+	Route::post('/like/add', [LikeController::class, 'index'])->name('like.store');
+	Route::post('/like/remove', [LikeController::class, 'destroy'])->name('like.destroy');
 });
