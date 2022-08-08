de49a640dc66796916746cb844212d910d66f30c
luka <lukakurdadze2@gmail.com>
2022-08-08 18:37:44 +0400

    fix: swagger not being able to upload image

--- app/Http/Controllers/EmailVerificationController.php
+++ app/Http/Controllers/EmailVerificationController.php
@@ -9,13 +9,16 @@ use Illuminate\Http\Request;
 
 class EmailVerificationController extends Controller
 {
-	public function verify(User $user, Request $request): JsonResponse|RedirectResponse
+	public function verify($user_id, Request $request): JsonResponse|RedirectResponse
 	{
 		if (!$request->hasValidSignature())
 		{
 			return response()->json('Invalid/Expired url provided.', 401);
 		}
-		elseif (!$user->hasVerifiedEmail())
+
+		$user = User::find($user_id);
+
+		if (!$user->hasVerifiedEmail())
 		{
 			$user->markEmailAsVerified();
 		}
